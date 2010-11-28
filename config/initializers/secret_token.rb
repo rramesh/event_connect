6d61358d3280732c4ce913787a89316039f41977
Ramesh Rajamani <rameshr@thoughtworks.com>
2010-11-28 11:06:00 +0530

    Initial app creation with simple meeting table for storing meeting information

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventConnect::Application.config.secret_token = '2f9741236617b96507bda7213707978aabbcbf0c44fd85efd77debf10151cba7bb3b406bad5d52e4722f018ee84f58db3bce8989c23fd1d744b20da434ab6153'
